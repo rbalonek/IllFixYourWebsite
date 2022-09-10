a06a55302f845d5a814cc1952748c41328c9f1cf
Robert Balonek <robertbalonek@gmail.com>
2022-09-10 14:05:27 -0700

    EmailJS Setup

--- src/screens/Services.jsx
+++ src/screens/Services.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { EmailForm } from '../components/EmailForm'
 
 export default function Services() {
   return (
     <div>
-    <h1>Services</h1>
+      <h1>Services</h1>
+      <EmailForm />
     </div>
   )
 }
