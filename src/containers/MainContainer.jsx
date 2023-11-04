f6729db8dd4a2be4a8d1c6fbd78f52bbd73729a8
Bob Balonek <rdevbob@gmail.com>
2023-11-04 12:15:09 -0700

    Test Btn

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import About from '../screens/About';
 import Home from '../screens/Home';
 import Services from '../screens/Services';
+import FeatureTestScreen from '../screens/FeatureTestScreen';
 
 export default function MainContainer() {
   return (
     <Routes>
     <Route path="/services" element={<Services />} />
-    <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/feature-test" element={<FeatureTestScreen />} />
     <Route path="/" element={<Home />} />
     </Routes>
   )
---
+++ src/screens/FeatureTestScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FeatureTestScreen() {
+  return (
+    <div className='text-white min-h-screen flex flex-col align-center justify-center items-center '>
+    <button id="test-purchase-button" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-2/3 rounded mt-5 shadow-md shadow-black contact-btn'>Purchase</button>
+    </div>
+  )
+}
